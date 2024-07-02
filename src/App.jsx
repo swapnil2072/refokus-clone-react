2e0b595a1ad76a7b9be2524cc6180863872e5754
swapnil2072 <swapnilshinde909693@gmail.com>
2024-07-02 13:17:16 +0530

    create nav component

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Navbar from "./components/Navbar";
+
+const App = () => {
+  return (
+    <div className="w-full h-screen bg-zinc-900 font-['satoshi'] text-white">
+      <Navbar></Navbar>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/Button.jsx
@@ -0,0 +1,9 @@
+const Button = () => {
+  return (
+    <div className="min-w-40 px-4 py-2 bg-zinc-100 text-black rounded-full">
+      <span className="text-sm font-medium">Get Started</span>
+    </div>
+  );
+};
+
+export default Button;
