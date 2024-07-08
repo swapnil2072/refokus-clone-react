54b997fa814fbc58d2bee8310bde1b85e57e55d3
swapnil2072 <swapnilshinde909693@gmail.com>
2024-07-08 22:48:54 +0530

    added locomotive and using locomotive we made website smooth

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,10 @@ import Marquees from "./components/Marquees";
 import Cards from "./components/Cards";
 import Footer from "./components/Footer";
 
+import LocomotiveScroll from "locomotive-scroll";
+
 const App = () => {
+  const locomotiveScroll = new LocomotiveScroll();
   return (
     <div className="w-full max-h-fit bg-zinc-900 font-['satoshi'] text-white  ">
       <Navbar />
